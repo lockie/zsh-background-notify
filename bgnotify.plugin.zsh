65c0928bd81d7195b19c7c31f3259b35456d40d3
Andrew Kravchuk <awkravchuk@gmail.com>
2024-04-22 09:30:40 +0200

    Fixed XMonad incompatibility

--- bgnotify.plugin.zsh
+++ bgnotify.plugin.zsh
@@ -26,7 +26,7 @@ currentWindowId () {
   if hash osascript 2>/dev/null; then #osx
     osascript -e 'tell application (path to frontmost application as text) to id of front window' 2&> /dev/null || echo "0"
   elif (hash notify-send 2>/dev/null || hash kdialog 2>/dev/null); then #ubuntu!
-    xprop -root 2> /dev/null | awk '/NET_ACTIVE_WINDOW/{print $5;exit} END{exit !$5}' || echo "0"
+    xprop -root 2> /dev/null | awk '/NET_ACTIVE_WINDOW\(WINDOW\)/{print $5;exit} END{exit !$5}' || echo "0"
   else
     echo $EPOCHSECONDS #fallback for windows
   fi
